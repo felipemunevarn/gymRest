5bce01692228b429a31f2171672f79c8773046a9
Felipe Munevar N <felipemunevarn@gmail.com>
2025-05-29 00:16:51 -0300

    creating profiles and environments in springboot

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,4 @@
+MERGE INTO training_types (type) KEY (type) VALUES ('CARDIO');
+MERGE INTO training_types (type) KEY (type) VALUES ('STRENGTH');
+MERGE INTO training_types (type) KEY (type) VALUES ('FLEXIBILITY');
+MERGE INTO training_types (type) KEY (type) VALUES ('HIIT');
